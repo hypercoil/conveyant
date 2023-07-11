536f179a2269544f4ece4f488695f8981ba4b321
Rastko Ciric <rastko@stanford.edu>
2023-07-11 14:19:37 -0700

    (REF) compositors in their own submodule

--- conveyant/__init__.py
+++ conveyant/__init__.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+from .compositors import (
+    direct_compositor,
+    close_imapping_compositor,
+    close_omapping_compositor,
+    delayed_outer_compositor,
+)
 from .flows import (
     ichain,
     ochain,
     iochain,
     split_chain,
     joindata,
-    direct_compositor,
-    close_imapping_compositor,
-    close_omapping_compositor,
     imapping_composition,
     omapping_composition,
     imap,
